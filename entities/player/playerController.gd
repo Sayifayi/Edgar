e5b9be6bdfe8ae65c61cfeadaad4140c93310e05
David <chorlo_95@hotmail.com>
2022-11-14 15:20:38 -0500

    Deleted redundat scripts
    Simplified player control onto a single script "Player Controller"

--- entities/player/playerController.gd
+++ entities/player/playerController.gd
@@ -1,36 +1,79 @@
-extends Node2D
+extends KinematicBody2D
 
-onready var nav2D : Navigation2D = $Navigation2D
-onready var player : KinematicBody2D = $player
+var path : PoolVector2Array
+onready var nav2D : Navigation2D = $"../Navigation2D"
 
+export var speed = 200
+
+#Defines default state and possible player states
 enum{IDLE, MOVE, INTERACT}
-
+var state = IDLE
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	var move_distance = speed * delta
+	
+	#Executes State behavior
+	match state:
+		IDLE:
+			pass
+		MOVE:
+			movePath(move_distance)
 
-#handles player on click movement input
+
+#Defines player movement
+func movePath(move_distance):
+	
+	var startingPoint : = position
+	
+	#Flips sprite x axis
+	if (startingPoint.x < path[0].x):
+		get_node("playerSprite").set_flip_h(false)
+	if (startingPoint.x > path[0].x):
+		get_node("playerSprite").set_flip_h(true)
+	
+	#Iterates upon path created on player controller
+	for i in range (path.size()):
+		var distance_to_next = startingPoint.distance_to(path[0])
+		 
+		#Interpolates position coordinates between current and next point in path array
+		if (move_distance <= distance_to_next):
+			position  = startingPoint.linear_interpolate(path[0], move_distance / distance_to_next)
+			break
+		#Clears array
+		path.remove(0)
+		
+		#Ends movement state
+		if (path.size() == 0):
+			changeState(IDLE)
+			
+			
+func changeState(newState):
+	state = newState
+	match state:
+		IDLE:
+			pass
+		MOVE:
+			pass
+	
+
 func _input(event):
 	if !Input.is_action_pressed("ui_leftMouseClick"):
 		return
 	#Creates a nav2D path between player current position and mouse position on click
-	var new_path = nav2D.get_simple_path(player.get_global_position(), get_global_mouse_position())
+	var new_path = nav2D.get_simple_path(self.get_global_position(), get_global_mouse_position())
 	
 	#sets path for player node
-	player.path = new_path
+	path = new_path
 	
 	#Triggers player Move state
-	player.changeState(MOVE)
+	changeState(MOVE)
 	
 
-#Defines behavior for interactables on click
-func _on_Interactable_input_event(viewport, event, shape_idx):
-	if !Input.is_action_pressed("ui_leftMouseClick"):
-		return
+
